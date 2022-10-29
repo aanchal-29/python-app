aa4bc02b8d8724941dd4286067f5d1418405d60a
Aanchal Gupta <b200047@nitsikkim.ac.in>
2022-10-29 17:56:35 +0530

    first commit

--- app.py
+++ app.py
@@ -7,7 +7,7 @@ app=Flask(__name__)
 #CONFIG
 #db = yaml.load(open('db.yaml'))
 db = yaml.safe_load(open('db.yaml'))
-print(db)
+
 app.config['MYSQL_HOST']= db['mysql_host']
 app.config['MYSQL_USER']= db['mysql_user']
 app.config['MYSQL_PASSWORD']= db['mysql_password']
@@ -18,40 +18,42 @@ mysql = MySQL(app)
 @app.route('/student')
 def student():
     
-    cur = mysql.connection.cursor()
-    resultValue = cur.execute("SELECT * FROM student")
-    if resultValue>0:
-        userDetails = cur.fetchall()
-        return render_template('student.html',userDetails=userDetails)
-    mysql.connection.commit()
-    cur.close
+   cur = mysql.connection.cursor()
+   resultValue = cur.execute("SELECT * FROM student")
+   print("hi executed DB fetch")
+   if resultValue>0:
+     userDetails = cur.fetchall()
+     return render_template('student.html',userDetails=userDetails)
+    
+    # return redirect('/student/update')    
+
+
+# @app.route('/update')
+# def update():
+    
+#      return render_template('index.html')
     
 
-    return redirect('/Modify')    
 
-
-
-@app.route('/Modify',methods=['GET','POST'])
+@app.route('/up',methods = ['GET','POST'])
 def index():
     
     if request.method =='POST':
         
        
         userDetails = request.form
-
-        name = userDetails['Name']
-        cur = mysql.connection.cursor()
-
-        inst= "select * FROM student where PaperType=Test"
-        sql = "INSERT INTO inst VALUES (%s)"
-
-        cur.execute(sql, name)
-        #  cur.execute ( "INSERT INTO student (select RollNo,Name,PaperType where PaperType="Test" ) )
-         #VALUES(%s)",(name)
+        print(userDetails)
+        name = userDetails['name']
+        print(name)
+        cur = mysql.connection.cursor()               
+        sql = "UPDATE student SET PaperType ='%s' WHERE PaperType = '%s'" % (name,"Test")        
+        cur.execute (sql)
         mysql.connection.commit()
-        cur.close
+        cur.close()
         return 'success'
     return render_template('index.html')
+
+    
     
 if __name__ == '__main__':
     app.run(debug=True)
